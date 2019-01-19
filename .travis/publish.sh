3b7aca41cb2f0ba54008c5d1b51f3711389a3f1b
Roberto Huertas <roberto.huertas@outlook.com>
2019-01-19 21:59:42 +0100

    fix script

--- .travis/publish.sh
+++ .travis/publish.sh
@@ -3,9 +3,9 @@ mkdir -p .pub-cache
 
 cat <<EOF > ~/.pub-cache/credentials.json
 {
-  "accessToken":"$accesstoken",
-  "refreshToken":"$refreshtoken",
-  "tokenEndpoint":"$tokenendpoint",
+  "accessToken":"$accessToken",
+  "refreshToken":"$refreshToken",
+  "tokenEndpoint":"$tokenEndpoint",
   "scopes":["https://www.googleapis.com/auth/plus.me","https://www.googleapis.com/auth/userinfo.email"],
   "expiration":$expiration
 }
