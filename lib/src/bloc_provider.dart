bf9ad9a111a51f4737214453da1ec18786946805
Roberto Huertas <roberto.huertas@outlook.com>
2021-01-04 17:48:11 +0100

    feat(api): document and remove deprecated api methods

--- lib/src/bloc_provider.dart
+++ lib/src/bloc_provider.dart
@@ -1,13 +1,46 @@
 import 'package:flutter/material.dart';
 import 'bloc.dart';
 
+/// Function used to customize the update policy
 typedef UpdateShouldNotify<T> = bool Function(T bloc, _BlocProvider oldWidget);
 
+/// The `BlocProvider` class depends on `InheritedWidget`.
+/// It accepts a bloc and a widget.
 class BlocProvider<T extends Bloc> extends StatefulWidget {
+  /// The  BLoC this provider will be hosting
   final T bloc;
+
+  /// The widget that the [BlocProvider] will wrap
   final Widget child;
+
+  /// Allows you to override the default update policy
+  ///
+  /// Default implementation:
+  ///
+  /// ```dart
+  ///    @override
+  ///    bool updateShouldNotify(_BlocProvider oldWidget) =>
+  ///       updateShouldNotifyOverride != null
+  ///           ? updateShouldNotifyOverride(bloc, oldWidget)
+  ///           : oldWidget.bloc != bloc;
+  /// ```
   final UpdateShouldNotify<T> updateShouldNotifyOverride;
 
+  /// Builds a [BlocProvider].
+  ///
+  /// [child] is the widget that the [BlocProvider] will wrap.
+  /// [bloc] is the BLoC this provider will be hosting.
+  /// [updateShouldNotifiyOverride] is an optional parameter that will allow you
+  /// to override the default behaviour.
+  /// This is the default implementation of the `updateShouldNotify` method:
+  ///
+  /// ```dart
+  ///    @override
+  ///    bool updateShouldNotify(_BlocProvider oldWidget) =>
+  ///       updateShouldNotifyOverride != null
+  ///           ? updateShouldNotifyOverride(bloc, oldWidget)
+  ///           : oldWidget.bloc != bloc;
+  /// ```
   BlocProvider({
     Key key,
     @required this.child,
@@ -15,10 +48,15 @@ class BlocProvider<T extends Bloc> extends StatefulWidget {
     this.updateShouldNotifyOverride,
   }) : super(key: key);
 
+  ////// Whenever you want to get your `BloC`, you can decide wether to attach the context of your widget to the `InheritedWidget` or not.
+  /// In order to control this behavior, the static method `of` has an optional boolean argument (which is true by default) which determines wether your context will be attached or not.
+  /// Basically, if you don't provide it or you just set it to `true`, [dependOnInheritedWidgetOfExactType](https://api.flutter.dev/flutter/widgets/BuildContext/dependOnInheritedWidgetOfExactType.html) will be used.
+  /// If you set it to `false` then [getElementForInheritedWidgetOfExactType](https://api.flutter.dev/flutter/widgets/BuildContext/getElementForInheritedWidgetOfExactType.html) will be used instead.
   static T of<T extends Bloc>(BuildContext context,
           [bool attachContext = true]) =>
       _BlocProvider.of(context, attachContext);
 
+  /// Creates the state
   @override
   _BlocProviderState<T> createState() => _BlocProviderState<T>();
 }
@@ -52,13 +90,21 @@ class _BlocProvider<T extends Bloc> extends InheritedWidget {
   }) : super(child: child);
 
   static T of<T extends Bloc>(BuildContext context, bool attachContext) {
-    final type = _typeOf<_BlocProvider<T>>();
-    final blocProvider = attachContext
-        ? (context.inheritFromWidgetOfExactType(type) as _BlocProvider)
-        : (context.ancestorInheritedElementForWidgetOfExactType(type).widget
-            as _BlocProvider);
+    _BlocProvider<T> blocProvider;
+
+    if (attachContext) {
+      blocProvider =
+          context.dependOnInheritedWidgetOfExactType<_BlocProvider<T>>();
+    } else {
+      var element =
+          context.getElementForInheritedWidgetOfExactType<_BlocProvider<T>>();
+      if (element != null) {
+        blocProvider = element.widget as _BlocProvider<T>;
+      }
+    }
 
     if (blocProvider == null) {
+      final type = _typeOf<_BlocProvider<T>>();
       throw FlutterError('Unable to find BLoC of type $type.\n'
           'Context provided: $context');
     }
